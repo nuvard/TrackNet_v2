8b4feb7bd33a80ac8adf26b08782a0836e683ea7
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2019-01-21 21:25:51 +0300

    tracknet model builder

---
+++ src/tracknet.py
@@ -0,0 +1,49 @@
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Conv1D
+from keras.layers import GRU
+from keras.layers import Dense
+from keras.layers import Masking
+from keras.layers import Activation
+from keras.layers import TimeDistributed
+from keras.layers import BatchNormalization
+from keras.layers import Concatenate
+from keras.activations import softplus
+
+
+def build_tracknet_model(n_features):
+    '''Build TrackNet_v2 model based on the input_shape
+
+    # Arguments
+        input_shape : the size of the input tensor
+            input tensor [N, D] 
+            N = sequence length, 
+            D = 4 - x, y, z, z+1 coords 
+    
+    # Returns 
+        Keras Model 
+    '''
+    # `None` means that an input sequence can be an arbitrary length
+    input_shape = (None, n_features)
+    inputs = Input(shape=input_shape, name="inputs")
+    # encode each timestep independently skipping zeros strings
+    x = TimeDistributed(
+        Masking(mask_value=0., name="mask"), 
+        name="time_distributed_mask")(inputs)
+    # timesteps encoder layer
+    x = Conv1D(32, 3, padding='same', name="conv1d")(x)
+    x = BatchNormalization(name='batch_norm')(x)
+    x = Activation('relu', name='conv1d_relu')(x)
+    # recurrent layers
+    x = GRU(32, return_sequences=True, name="gru1")(x)
+    x = GRU(16, name="gru2")(x)
+    # outputs
+    # x and y coords - centre of observing
+    # area on the next station
+    xy_coords = Dense(2, activation='linear', name="xy_coords")(x)
+    # ellipse radii
+    r1_r2 = Dense(2, activation=softplus, name="r1_r2")(x)
+    outputs = Concatenate(name="outputs")([xy_coords, r1_r2])
+    # create model
+    tracknet = Model(inputs, outputs, name="TrackNet_v2")
+    return tracknet
